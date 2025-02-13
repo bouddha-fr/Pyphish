77799981dfa9d323e882392f716760c5e8ec3915
bouddha-fr <bouddha.whitehat@gmail.com>
2025-02-13 12:59:52 +0100

    config pyphish.py

--- src/app/pyphish.py
+++ src/app/pyphish.py
@@ -6,4 +6,5 @@ app = Flask(__name__)
 def hello_world():
     return "Hello, World!"
 
-app.run(host="127.0.0.1", port=8080, debug=True)
+if __name__ == "__main__":
+    app.run(host="127.0.0.1", port=8080, debug=True)
