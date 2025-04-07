9e336f5a0737e81f090260c567495ca06c6771af
bouddha-fr <bouddha.whitehat@gmail.com>
2025-04-07 16:55:59 +0200

    great merge

--- src/app/pyphish.py
+++ src/app/pyphish.py
@@ -1,10 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask
+from src.app.routes.routes import register_routes
 
 app = Flask(__name__)
-
-@app.route("/")
-def hello_world():
-    return render_template('index.html')
+register_routes(app)
 
 if __name__ == "__main__":
     app.run(host="127.0.0.1", port=8080, debug=True)
---
+++ src/app/routes/routes.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+def register_routes(app: Flask):
+    @app.route("/")
+    def hello_world():
+        return render_template('index.html')
+    @app.route("/pydashboard")
+    def dashboard():
+        return render_template('pydashboard.html')
