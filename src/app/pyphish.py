967760d476f7cf780d7be2b01aec1a6fe0ad11aa
bouddha-fr <bouddha.whitehat@gmail.com>
2025-04-08 09:01:41 +0200

    add env

--- src/app/pyphish.py
+++ src/app/pyphish.py
@@ -1,8 +1,11 @@
 from flask import Flask
 from src.app.routes.routes import register_routes
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__)
 register_routes(app)
 
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=8080, debug=True)
+    app.run(host="127.0.0.1", port=8000, debug=True)
