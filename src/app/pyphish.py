3ba9f4441b60ea00a20f44a6d913672b0fa49b6b
bouddha-fr <bouddha.whitehat@gmail.com>
2025-04-08 10:40:02 +0200

    great merge

--- src/app/pyphish.py
+++ src/app/pyphish.py
@@ -1,8 +1,17 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 from src.app.routes.routes import register_routes
 
 app = Flask(__name__)
-register_routes(app)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mariadb+mariadbconnector://user:password@localhost:3306/pyphish'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+app.config['SECRET_KEY'] = 'motdepasse'
+
+db = SQLAlchemy(app)
+
+register_routes(app, db)
 
 if __name__ == "__main__":
     app.run(host="127.0.0.1", port=8000, debug=True)
--- src/app/routes/routes.py
+++ src/app/routes/routes.py
@@ -1,11 +1,36 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for, flash, session
+from src.app.models import User
+from werkzeug.security import check_password_hash
+from src.app import db
 
-app = Flask(__name__)
-
-def register_routes(app: Flask):
+def register_routes(app: Flask, db):
     @app.route("/")
     def hello_world():
         return render_template('index.html')
+
     @app.route("/connexion")
-    def dashboard():
+    def connexion():
         return render_template('pyconnexion.html')
+
+    @app.route("/login", methods=["POST"])
+    def login():
+        username = request.form.get("username")
+        password = request.form.get("password")
+
+        # Chercher l'utilisateur dans la base de données
+        user = User.query.filter_by(username=username).first()
+
+        if user and user.check_password(password):
+            # Utilisateur authentifié, on démarre une session
+            session["user_id"] = user.id
+            flash("Connexion réussie!", "success")
+            return redirect(url_for("dashboard"))  # Redirige vers une page protégée, comme un dashboard
+        else:
+            flash("Nom d'utilisateur ou mot de passe incorrect", "danger")
+            return redirect(url_for("connexion"))
+
+    @app.route("/dashboard")
+    def dashboard():
+        if "user_id" not in session:
+            return redirect(url_for("connexion"))  # Redirige si l'utilisateur n'est pas connecté
+        return render_template('dashboard.html')  # Page protégée accessible après connexion
