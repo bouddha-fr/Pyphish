d1049412aeedfde215d9e6c6e1e00a7f08b0ed7e
Kiri <bryan.corret@gmail.com>
2025-04-09 12:47:07 +0200

    backend

--- src/app/pyphish.py
+++ src/app/pyphish.py
@@ -1,12 +1,12 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from src.app.routes.routes import register_routes
-from src.app.models import db  # Si db est défini dans models, on l'importera ici
+from app.routes.routes import register_routes
+from app.models import db  # Si db est défini dans models, on l'importera ici
 
 app = Flask(__name__)
 
 # Configuration de l'application Flask
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mariadb+mariadbconnector://root:Motdepasse@172.16.27.135:3306/pyphish'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mariadb+mariadbconnector://root:Motdepasse@172.16.160.132:3306/pyphish'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = 'Motdepasse'
 
@@ -17,4 +17,4 @@ db.init_app(app)
 register_routes(app, db)
 
 if __name__ == "__main__":
-    app.run(host="172.16.27.135", port=8000, debug=True)
+    app.run(host="127.0.0.1", port=8000, debug=True)
